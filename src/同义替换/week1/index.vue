df0933cd8dc8be0ff755bf3f442d890bf027c665
leafjava <a894074295@gmail.com>
2024-04-24 15:55:03 +0800

    完善

--- src/同义替换/week1/index.vue
+++ src/同义替换/week1/index.vue
@@ -170,7 +170,9 @@
 <script>
 import {ref,computed,onMounted,watch} from "vue"
 // import word from '../week2/day8听力同义替换.json';
-import word from '../week2/day9同义替换.json';
+// import word from '../week2/day9同义替换.json';
+// import word from '../week2/day11阅读同义替换.json';
+import word from '../week2/day13阅读同义替换.json';
 
 export default {
   components:{
